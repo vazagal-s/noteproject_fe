880d7192099b7e4be252b1af505bfc6bd83dfcb1
vazagal-s <caio.assis@unesp.br>
2025-07-01 13:34:49 -0300

    Novo endereço da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.19.0.8:8082/api",
+  baseURL: "http://api.notes4web.vzgtech.com.br/api",
 });
 
 api.interceptors.request.use((config) => {
